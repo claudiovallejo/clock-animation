9a0bcad29f142751cb03e5684345a819750b084b
Claudio Vallejo <hola@claudiovallejo.mx>
2016-04-14 16:33:40 -0500

    Finished the tutorial. Have the code needed to incorporate the animating clock into the svg clock icon

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded', displayTime);
+document.addEventListener('DOMContentLoaded', startTimer);
+
+function startTimer() {
+  setInterval(displayTime, 1000);
+  displayTime();
+
+}
 
 function displayTime() {
   var now = new Date();
@@ -9,6 +15,47 @@ function displayTime() {
   var timeString = formatHour(h) + ":" + padZero(m) + ":" + padZero(s) + " " + getTimePeriod(h);
 
   document.getElementById('current-time').innerHTML = timeString;
+
+  // --- Analog clock ---//
+  var canvas = document.querySelector("#clock");
+  var context = canvas.getContext("2d");
+
+  // You can change this to make the clock as big or small as you want.
+  // Just remember to adjust the canvas size if necessary.
+  var clockRadius = 50;
+
+  // Make sure the clock is centered in the canvas
+  var clockX = canvas.width / 2;
+  var clockY = canvas.height / 2;
+
+  // Make sure TAU is defined (it's not by default)
+  Math.TAU = 2 * Math.PI;
+
+  function drawArm(progress, armThickness, armLength, armColor) {
+    var armRadians = (Math.TAU * progress) - (Math.TAU/4);
+    var targetX = clockX + Math.cos(armRadians) * (armLength * clockRadius);
+    var targetY = clockY + Math.sin(armRadians) * (armLength * clockRadius);
+
+    context.lineWidth = armThickness;
+    context.strokeStyle = armColor;
+
+    context.beginPath();
+    context.moveTo(clockX, clockY);
+    context.lineTo(targetX, targetY);
+    context.stroke();
+
+  }
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawArm(h/12, 2, 0.50, '#b3b3b3');
+  drawArm(m/60, 2, 0.75, '#b3b3b3');
+
+  // Create a circle at the center of the clock
+  context.beginPath();
+  context.arc(clockX, clockY, 3, 0, Math.PI*2);
+  context.fillStyle = '#b3b3b3';
+  context.fill();
 
 }
 
