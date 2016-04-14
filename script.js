b15388878c1d78ce180ae8135c7b50586ec13f89
Claudio Vallejo <hola@claudiovallejo.mx>
2016-04-14 14:40:41 -0500

    Added index.css (which contains the basic styling of the Javascript message), and added script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', displayTime);
+
+function displayTime() {
+  //Just to test if the JavaScript gets loaded properly. We will remove this later.
+  document.getElementById('current-time').innerHTML = "JavaScript is working, now get to coding."
+
+}
