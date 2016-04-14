72a1f9d5722fb20bbfca78bc4b2cb2234f97a22d
Claudio Vallejo <hola@claudiovallejo.mx>
2016-04-14 14:52:12 -0500

    Added formatHour() and getTimePeriod() functions

--- script.js
+++ script.js
@@ -6,8 +6,36 @@ function displayTime() {
   var m = now.getMinutes();
   var s = now.getSeconds();
 
-  var timeString = String(h) + ":" + String(m) + ":" + String(s);
+  var timeString = formatHour(h) + ":" + padZero(m) + ":" + padZero(s) + " " + getTimePeriod(h);
 
   document.getElementById('current-time').innerHTML = timeString;
+
+}
+
+function padZero(num) {
+  if (num < 10) {
+    return "0" + String(num);
+
+  } else {
+    return String(num);
+
+  }
+
+}
+
+function formatHour(h) {
+  var hour = h % 12;
+
+  if (hour == 0) {
+    hour = 12;
+
+  }
+
+  return String(hour)
+
+}
+
+function getTimePeriod(h) {
+  return (h < 12) ? "AM" : "PM";
 
 }
